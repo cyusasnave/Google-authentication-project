ebc7a1b4fa6c4433f4c640de27cc1a61a772f7bb
Hakizimana Clement <h.clementoert@gmail.com>
2024-04-10 20:49:44 +0200

    added login and logout controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response, response } from "express";
+import { Request, Response } from "express";
 import { GoogleUserModel } from "../database/models/user.model";
 import { GoogleUserModelAttributes } from "../database/models/user.model";
 import { generateAccessToken } from "../helpers/security.helpers";
@@ -8,27 +8,53 @@ const handleGoogleAuth = async (req: Request, res: Response) => {
     if (req.user) {
       const userProfile = req.user as GoogleUserModelAttributes;
 
-      const isUserExist = await GoogleUserModel.findOne({
+      const UserExist = await GoogleUserModel.findOne({
         where: { googleId: userProfile.googleId },
       });
+      // LOGIN
+      if (UserExist) {
+        const userData = UserExist.dataValues;
+        const isVerified = userData.isVerified;
 
-      if (isUserExist) {
-        return res
-          .status(409)
-          .json({ status: "confict", message: "User already exist!!" });
+        if (!isVerified) {
+          return res
+            .status(403)
+            .json({ message: "please verfiy you're email" });
+        }
+
+        const { id, googleId, email, role } = userData;
+        const token = generateAccessToken({ id, googleId, email, role });
+
+        return res.status(201).json({ message: "created", token: token });
+      } else {
+        // CREATE
+        const newUser = await GoogleUserModel.create({ ...userProfile });
+        await newUser.save();
+
+        res.status(201).json({ message: "created" });
       }
-
-      const newUser = await GoogleUserModel.create({ ...userProfile });
-      await newUser.save();
-
-      const { id, googleId, email, role } = newUser.dataValues;
-
-      const token = generateAccessToken({ id, googleId, email, role });
-      res.status(201).json({ message: "created", token: token });
     }
   } catch (error) {
     res.status(500).json({ message: "Something went wrong", error: error });
   }
 };
 
-export default { handleGoogleAuth };
+const userDashboard = async (req: Request, res: Response) => {
+  const userProfile = req.user as GoogleUserModelAttributes;
+  res
+    .status(200)
+    .send(
+      `<h1>Welcome to Dashboard ${userProfile.firstName} ${userProfile.lastName} 🎉</h1> <br> <a href="/users/auth/google/logout">Logout</a>`
+    );
+};
+
+const userLogout = async (req: Request, res: Response, done: any) => {
+  req.logout(done);
+  res.redirect("/");
+};
+
+export default {
+  handleGoogleAuth,
+  userDashboard,
+  userLogout,
+};
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,19 +1,28 @@
 import express from "express";
 import passport = require("passport");
 import authController from "../controllers/auth.controller";
+import authCheck from "../middlewares/authCheck";
+
 const authRouter = express.Router();
 
-authRouter
-  .get(
-    "/",
-    passport.authenticate("google", {
-      scope: ["profile", "email"],
-    })
-  )
-  .get(
-    "/callback",
-    passport.authenticate("google", { failureRedirect: "/" }),
-    authController.handleGoogleAuth
-  );
+authRouter.get(
+  "/",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+authRouter.get(
+  "/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  authController.handleGoogleAuth
+);
+authRouter.get("/dashboard", authCheck, authController.userDashboard);
+authRouter.get("/logout", authController.userLogout);
+authRouter.get(
+  "/login",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
 
 export default authRouter;
